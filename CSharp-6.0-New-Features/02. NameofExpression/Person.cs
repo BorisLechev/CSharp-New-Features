b96ed2225b48255308fabbefe99c5c6f6752db9e
BorisLechev <b.k.lechev@gmail.com>
2021-12-16 20:09:48 +0200

    Add 02. NameofExpression

---
+++ CSharp-6.0-New-Features/02. NameofExpression/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._NameofExpression
+{
+    public class Person
+    {
+        private readonly string firstName;
+
+        private readonly string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            this.lastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        }
+
+        public string Name => string.Format("{0} {1}", this.firstName, this.lastName);
+    }
+}
---
+++ CSharp-6.0-New-Features/02. NameofExpression/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _02._NameofExpression
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Name of static method
+            Console.WriteLine(nameof(Main));  // Main
+
+            try
+            {
+                var person = new Person(null, null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message); // Value cannot be null. (Parameter 'firstName')
+            }
+
+            // Name of instance property
+            new Program().NonStaticMethod(); // NonStaticProperty
+
+            // Name of methods
+            Console.WriteLine(nameof(string.Empty.Normalize)); // Normalize
+        }
+
+        public int NonStaticProperty { get; set; }
+
+        public void NonStaticMethod()
+        {
+            Console.WriteLine(nameof(this.NonStaticProperty));
+        }
+    }
+}

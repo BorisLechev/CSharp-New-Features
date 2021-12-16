c961efe2898c09a0f6056c1bef67780121b46123
BorisLechev <b.k.lechev@gmail.com>
2021-12-16 19:14:19 +0200

    Add 01. UsingStatic

---
+++ CSharp-6.0-New-Features/01. UsingStatic/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using static System.Math;
+using static System.Linq.Enumerable;
+
+namespace _01._UsingStatic
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            WriteLine(Pow(2, 10)); // Instead of Console.WriteLine(Math.Pow(2, 10))
+
+            Range(1, 10).ToList().ForEach(WriteLine); // Using static in combination with extension methods
+        }
+    }
+}

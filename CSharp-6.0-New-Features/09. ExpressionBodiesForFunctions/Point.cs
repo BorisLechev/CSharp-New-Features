adf6306b0a6abbc84d3b8bf712a43e5998778112
BorisLechev <b.k.lechev@gmail.com>
2021-12-17 15:18:30 +0200

    Add 09. ExpressionBodiesForFunctions

---
+++ CSharp-6.0-New-Features/09. ExpressionBodiesForFunctions/Point.cs
@@ -0,0 +1,37 @@
+﻿namespace _09._ExpressionBodiesForFunctions
+{
+    public class Point : ICloneable
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        // Before:
+        ////public void Print()
+        ////{
+        ////    Console.WriteLine(this.X + " " + this.Y);
+        ////}
+        // Now:
+        public void Print() => Console.WriteLine(this.X + " " + this.Y);
+
+        // Before:
+        ////public object Clone() // Returns a new object that is a copy of this instance
+        ////{
+        ////    return new Point(this.X, this.Y);
+        ////}
+        // Now:
+        public object Clone() => new Point(this.X, this.Y); // Returns a new object that is a copy of this instance
+
+        public void Move(int x, int y)
+        {
+            this.X += x;
+            this.Y += y;
+        }
+    }
+}
---
+++ CSharp-6.0-New-Features/09. ExpressionBodiesForFunctions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09._ExpressionBodiesForFunctions
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var point1 = new Point(40, 80);
+            var point2 = point1.Clone() as Point;
+
+            point1.Move(100, 100);
+            point2.Print();
+            point1.Print();
+
+            point1.Move(200, 200);
+            point1.Print();
+        }
+    }
+}

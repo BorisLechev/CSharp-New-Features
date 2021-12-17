4a3ae17a0ec0d5322c3ef5eb5a958df0d2f76329
BorisLechev <b.k.lechev@gmail.com>
2021-12-17 14:29:39 +0200

    Add 07. IndexInitializers

---
+++ CSharp-6.0-New-Features/07. IndexInitializers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _07._IndexInitializers
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // C# 5.0
+            var numbersOldWay = new Dictionary<int, string>
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" },
+            };
+
+            // C# 6.0
+            var numbersNewWay = new Dictionary<int, string>
+            {
+                [1] = "one",
+                [2] = "two",
+                [3] = "three",
+            };
+
+            var dictionary = new Dictionary<string, string>
+            {
+                ["C#"] = "A programming language",
+                [".NET"] = "A development platform",
+                ["CIL"] = "Lowest-level programming language used by the .NET Framework",
+            };
+        }
+    }
+}

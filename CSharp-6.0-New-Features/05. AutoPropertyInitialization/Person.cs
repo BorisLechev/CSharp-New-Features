5d15f7b9586aed43618dc9a46dfb218655819b3a
BorisLechev <b.k.lechev@gmail.com>
2021-12-17 13:31:15 +0200

    Add 05. AutoPropertyInitialization

---
+++ CSharp-6.0-New-Features/05. AutoPropertyInitialization/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace _05._AutoPropertyInitialization
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = "Boris";
+
+        public string LastName { get; set; } = "Lechev";
+    }
+}
---
+++ CSharp-6.0-New-Features/05. AutoPropertyInitialization/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _05._AutoPropertyInitialization
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Initializers for auto-properties
+            var person = new Person();
+
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+}

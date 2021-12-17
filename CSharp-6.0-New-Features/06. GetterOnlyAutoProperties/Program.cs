6076ae77ca4b0c3d45b46f5b7c39093752b40e59
BorisLechev <b.k.lechev@gmail.com>
2021-12-17 13:49:51 +0200

    Add 06. GetterOnlyAutoProperties

---
+++ CSharp-6.0-New-Features/06. GetterOnlyAutoProperties/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _06._GetterOnlyAutoProperties
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var person = new ReadOnlyPerson("Boris");
+
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+}
---
+++ CSharp-6.0-New-Features/06. GetterOnlyAutoProperties/ReadOnlyPerson.cs
@@ -0,0 +1,15 @@
+﻿namespace _06._GetterOnlyAutoProperties
+{
+    public class ReadOnlyPerson
+    {
+        public ReadOnlyPerson(string firstName)
+        {
+            // This line is also valid for read-only property initialization
+            this.FirstName = firstName;
+        }
+
+        public string FirstName { get; } = "Boris";
+
+        public string LastName { get; } = "Lechev";
+    }
+}

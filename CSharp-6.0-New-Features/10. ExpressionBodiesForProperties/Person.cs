52ae569bc6ab756476c0ba227c5b8dd19d18f12d
BorisLechev <b.k.lechev@gmail.com>
2021-12-17 15:50:32 +0200

    Add 10. ExpressionBodiesForproperties

---
+++ CSharp-6.0-New-Features/10. ExpressionBodiesForProperties/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace _10._ExpressionBodiesForProperties
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Children = new List<Person>();
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<Person> Children { get; set; }
+
+        // Before:
+        //// public string Name
+        //// {
+        ////     get
+        ////     {
+        ////         return this.FirstName + " " + this.LastName;
+        ////     }
+        //// }
+        // Now:
+        public string Name => this.FirstName + " " + this.LastName;
+
+        // Expression for indexer body:
+        public Person this[string name] => 
+            this.Children.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+    }
+}
---
+++ CSharp-6.0-New-Features/10. ExpressionBodiesForProperties/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _10._ExpressionBodiesForProperties
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var person = new Person("Boris", "Lechev");
+
+            person.Children.Add(new Person("Ivan", "Ivanov"));
+
+            Console.WriteLine(person.Name); // Boris Lechev
+            Console.WriteLine(person["Ivan Ivanov"].Name); // Ivan Ivanov
+        }
+    }
+}

4848b93beaf97c3fb009baa29c8227f3fd670d06
BorisLechev <b.k.lechev@gmail.com>
2021-12-18 11:51:59 +0200

    Add Deconstruction

---
+++ CSharp-7.0-New-Features/1. Deconstruction/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _1._Deconstruction
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Deconstruct with Deconstruct() method
+            var person = new Person("John", "Smith", "Smith");
+
+            (string firstName, _, string lastName) = person;
+
+            Console.WriteLine($"{firstName} {lastName}");
+
+            // Deconstruct with extension method
+            (int year, int month, int day) = DateTime.Now;
+            Console.WriteLine($"{day}.{month}.{year}");
+        }
+
+        private static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day)
+        {
+            year = dateTime.Year;
+            month = dateTime.Month;
+            day = dateTime.Day;
+        }
+    }
+}
+

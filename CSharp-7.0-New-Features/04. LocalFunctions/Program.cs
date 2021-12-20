fb8b49b7728f350e6315795cf1dc5514cc008313
BorisLechev <b.k.lechev@gmail.com>
2021-12-20 21:54:14 +0200

    Add 04. LocalFunctions

---
+++ CSharp-7.0-New-Features/04. LocalFunctions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _04._LocalFunctions
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(ConcatenateStrings("a", "b", "c", "d"));
+        }
+
+        private static string ConcatenateStrings(string a, string b, string c, string d)
+        {
+            void Validate(string paramName, object param)
+            {
+                if (param == null)
+                {
+                    throw new ArgumentException($"{paramName} cannot be null.", paramName);
+                }
+            }
+
+            Validate(nameof(a), a);
+            Validate(nameof(b), b);
+            Validate(nameof(c), c);
+            Validate(nameof(d), d);
+
+            return $"{a} {b} {c} {d}";
+        }
+    }
+}

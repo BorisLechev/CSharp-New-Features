a412c199f5bc5f4dadb11943ecff6c204953ba3a
BorisLechev <b.k.lechev@gmail.com>
2021-12-21 17:06:29 +0200

    Add 06. NumberLiteralsImprovements

---
+++ CSharp-7.0-New-Features/06. NumberLiteralsImprovements/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _06._NumberLiteralsImprovements
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int a = 54_123_768;
+            int weird = 1_2__3___4____5;
+
+            Console.WriteLine($"{nameof(a)} = {a}"); // 54123768
+            Console.WriteLine($"{nameof(weird)} = {weird}"); // 12345
+        }
+    }
+}

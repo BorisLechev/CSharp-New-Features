f8c4e7cb118db2f0af3e882a8ca8da92e71e9473
BorisLechev <b.k.lechev@gmail.com>
2021-12-21 16:47:17 +0200

    Add 05. OutVariables

---
+++ CSharp-7.0-New-Features/05. OutVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._OutVariables
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Before C# 7.0
+            int a;
+            if (int.TryParse("123", out a))
+            {
+                Console.WriteLine(a);
+            }
+
+            // C# 7.0
+            if (int.TryParse("123", out int b))
+            {
+                Console.WriteLine(b);
+            }
+
+            // Infer the type of x
+            if (int.TryParse("123", out var c))
+            {
+                Console.WriteLine(c);
+            }
+
+            // Infer the type of x
+            if (int.TryParse("123", out _))
+            {
+                Console.WriteLine("Parsed successfully");
+            }
+        }
+    }
+}

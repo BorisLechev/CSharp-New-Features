7cbcfdae919e4c307be7c9d9dea2bad16ad6f3f2
BorisLechev <b.k.lechev@gmail.com>
2021-12-21 17:14:30 +0200

    Add 07. ThrowsExpressions

---
+++ CSharp-7.0-New-Features/07. ThrowExpressions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07._ThrowExpressions
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            PrintName("Boris"); // Boris
+            PrintName(null); // Exception
+        }
+
+        private static void PrintName(string name)
+        {
+            Console.WriteLine(name ?? throw new ArgumentNullException());
+        }
+    }
+}

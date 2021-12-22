835e2912d0b0dd1513f2b52fa4bbc3350041b1fe
BorisLechev <b.k.lechev@gmail.com>
2021-12-22 15:04:13 +0200

    Add 02. FileScopedNamespaces

---
+++ CSharp-10.0-New-Features/02. FileScopedNamespaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassesNamespace;
+
+namespace FileScopedNamespaces;
+
+public class Program
+{
+    public static void Main()
+    {
+        var testObject = new Test();
+        Console.WriteLine("Hello, World!");
+    }
+}

73820126cc1635e5531009229ad3b42a904e8903
BorisLechev <b.k.lechev@gmail.com>
2021-12-22 14:11:01 +0200

    Add 04.NativeSizedIntegers

---
+++ CSharp-9.0-New-Features/04. NativeSizedIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._NativeSizedIntegers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            unsafe
+            {
+                Console.WriteLine($"size of nint = {sizeof(nint)}");
+                Console.WriteLine($"size of nuint = {sizeof(nuint)}");
+
+                // output when run in a 64-bit process
+                //size of nint = 8
+                //size of nuint = 8
+
+                // output when run in a 32-bit process
+                //size of nint = 4
+                //size of nuint = 4
+            }
+        }
+    }
+}

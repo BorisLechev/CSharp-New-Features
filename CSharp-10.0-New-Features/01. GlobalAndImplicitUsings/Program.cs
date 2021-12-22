b5ba1478b0d10090fe2ef86a08442a3ba4c918cf
BorisLechev <b.k.lechev@gmail.com>
2021-12-22 14:51:45 +0200

    Add 01. GlobalAndImplicitUsings

---
+++ CSharp-10.0-New-Features/01. GlobalAndImplicitUsings/Program.cs
@@ -0,0 +1,10 @@
+﻿// System is included by default because of <ImplicitUsings>enable</ImplicitUsings> in project file
+Console.WriteLine(Math.Cos(Math.PI)); // -1
+
+// System.Text.RegularExpressions is included in the project file
+Console.WriteLine(Regex.Match("Hello, 2022!", @"\d{4}").Value); // 2022
+
+// System.Net.Http is disabled in the project file
+System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+Console.WriteLine(new Matrix3x2(1, 2, 3, 4, 5, 6).GetDeterminant()); // -2

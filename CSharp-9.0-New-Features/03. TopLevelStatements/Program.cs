f3a39cdfe7c5c71c333dcf281cd0d0548b729d56
BorisLechev <b.k.lechev@gmail.com>
2021-12-22 13:58:20 +0200

    Add 03. TopLevelStatements and Programs

---
+++ CSharp-9.0-New-Features/03. TopLevelStatements/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+Console.WriteLine("Hello World!");
+Console.WriteLine(Math.Pow(2, 10));
+
+// args is available with the command-line arguments
+Console.WriteLine($"I've got {args.Length} argument(s)."); // I've got 0 argument(s).
+
+// Tasks can also be used
+await Task.CompletedTask;
+
+// Return statement is also allowed
+return 0;

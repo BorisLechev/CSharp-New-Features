3f10d06c99a7f66a529172a07e48097f640d7328
BorisLechev <b.k.lechev@gmail.com>
2021-12-22 13:40:49 +0200

    Add 02. InitOnlyProperties

---
+++ CSharp-9.0-New-Features/02. InitOnlyProperties/Point.cs
@@ -0,0 +1,4 @@
+﻿namespace _02._InitOnlyProperties
+{
+    public record Point(int X, int Y);
+}
---
+++ CSharp-9.0-New-Features/02. InitOnlyProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._InitOnlyProperties
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var personWithConstructor = new Person("Mimi", 5);
+            Console.WriteLine(personWithConstructor); // Mimi is 5 years old.
+
+            var personWithInitialization = new Person { Name = "Mimi", Age = 15 };
+            Console.WriteLine(personWithInitialization); // Mimi is 15 years old.
+
+            var point = new Point(1, 2);
+            var mutatedPoint = point with { X = 2, Y = 3 };
+            Console.WriteLine(mutatedPoint.ToString()); // Point { X = 2, Y = 3 }
+        }
+    }
+}
